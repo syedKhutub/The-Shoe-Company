9f4ab268e64245ddfc5303a12282ec1e8c49ffc1
Syed Khutub <syedkhutubsks@gmail.com>
2021-12-02 04:11:39 +0530

    selected box

---
+++ src/components/SelectBox/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './css/index.css';
+
+const SelectBox = ({size}) => {
+    return (
+        <div className="selected-container">
+            {size}
+        </div>
+    )
+}
+export default SelectBox;
