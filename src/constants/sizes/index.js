64e9957de47200091a0344b09e7e0bdf96a8eba4
Syed Khutub <syedkhutubsks@gmail.com>
2021-12-02 04:38:23 +0530

    created sizes constants

---
+++ src/constants/sizes/index.js
@@ -0,0 +1,70 @@
+export const SIZE =  [
+    {
+      label: `35`,
+      value: '35',
+    },
+    {
+      label: `36`,
+      value: '36',
+    },
+    {
+      label: `37`,
+      value: '37',
+    },
+    {
+      label: `38`,
+      value: '38',
+    },
+    {
+      label: `39`,
+      value: '39',
+    },
+    {
+      label: `40`,
+      value: '40',
+    },
+    {
+      label: `41`,
+      value: '41',
+    },
+    {
+      label: `42`,
+      value: '42',
+    },
+    {
+      label: `43`,
+      value: '43',
+    },
+    {
+      label: `44`,
+      value: '44',
+    },
+    {
+      label: `45`,
+      value: '45',
+    },
+    {
+      label: `44`,
+      value: '44',
+    },
+    {
+      label: `45`,
+      value: '45',
+    },
+    {
+      label: `46`,
+      value: '46',
+    },
+    {
+      label: `47`,
+      value: '47',
+    },
+    {
+      label: `48`,
+      value: '48',
+    },
+    {
+      label: `49`,
+      value: '49',
+    }
+  ]
