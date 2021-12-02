b0f7ebb69f9b42f4901d5c7bddc0e53ef84084d2
Syed Khutub <syedkhutubsks@gmail.com>
2021-12-02 15:00:46 +0530

    modified to create shoes data

--- src/container/Dashboard/constants.js
+++ src/container/Dashboard/constants.js
@@ -2,6 +2,7 @@ export const ALL_SHOES = [
     {
         productName: 'Heeled Boots', 
         brandName: 'ZAPATOZ',
+        categogy: 'Boots',
         mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/877b9154-5a71-4919-9559-30c1cc5aa8781597959110028-1.jpg',
         price: 699,
         smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/7f1400a4-62d4-4c9e-8795-2e3ee80523541597959110116-3.jpg',
@@ -11,6 +12,7 @@ export const ALL_SHOES = [
     {
         productName: 'Zeta Long Distance', 
         brandName: 'Puma',
+        categogy: 'Sports',
         mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2021/6/4/fcb19968-5d47-4879-b337-6757e4e0ee5c1622826840336-1.jpg',
         price: 1864,
         smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2021/6/4/1e14238f-b8e9-42df-816f-78f07f17e8341622826840344-2.jpg',
@@ -20,6 +22,7 @@ export const ALL_SHOES = [
     {
         productName: 'Men Black Revolution', 
         brandName: 'Nike',
+        categogy: 'Sports',
         mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15384984/2021/10/5/d485408f-e410-48e6-aaf0-1b07a487a6f91633413329647-Nike-Revolution-5-Mens-Running-Shoes-361633413329251-1.jpg',
         price: 2699,
         smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15384984/2021/10/5/67729158-d94d-4eba-96b7-baea55d4c3331633413329640-Nike-Revolution-5-Mens-Running-Shoes-361633413329251-2.jpg',
@@ -29,6 +32,7 @@ export const ALL_SHOES = [
     {
         productName: 'Black Woven Design', 
         brandName: 'Fentacia',
+        categogy: 'Boots',
         mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15583000/2021/9/24/df2911f9-c809-4bc2-8bd5-17eafda8eec11632467705466FentaciaMenBlackPrintedFlatBoots1.jpg',
         price: 999,
         smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15583000/2021/9/24/56b132e1-61f5-4ad4-99b3-15058360cc311632467705940FentaciaMenBlackPrintedFlatBoots3.jpg',
@@ -38,6 +42,7 @@ export const ALL_SHOES = [
     {
         productName: 'Brown Derbys', 
         brandName: 'El Paso',
+        categogy: 'Formal',
         mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/9249461/2021/8/6/fbeaafe5-8382-4cbb-93f7-2b1108f4fc621628224519246-El-Paso-Men-Brown-Derbys-6811628224518661-1.jpg',
         price: 1499,
         smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/9249461/2021/8/6/57b503ce-d02b-4d5d-bac9-c5260877c04f1628224519203-El-Paso-Men-Brown-Derbys-6811628224518661-3.jpg',
@@ -47,6 +52,7 @@ export const ALL_SHOES = [
     {
         productName: 'Blue Mesh Air +', 
         brandName: 'Red Tape',
+        categogy: 'Sports',
         mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2021/6/28/40c45e54-c55f-4ac8-9c9b-7290752fb02b1624899622324-1.jpg',
         price: 2099,
         smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2021/6/28/bd108d89-de44-49cd-b70d-ebb7c13c936d1624899622344-3.jpg',
@@ -56,6 +62,7 @@ export const ALL_SHOES = [
     {
         productName: 'Tan Textured PU Loafers', 
         brandName: 'U.S. Polo Assn.',
+        categogy: 'Formal',
         mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15738700/2021/10/20/f4fd3e74-6f1e-4d02-b918-da02617fdad01634712463016-US-Polo-Assn-Men-Casual-Shoes-9161634712462603-1.jpg',
         price: 999,
         smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15738700/2021/10/20/a01fbb7c-665b-4172-ae3e-46d77a0c6a381634712463009-US-Polo-Assn-Men-Casual-Shoes-9161634712462603-2.jpg',
@@ -65,6 +72,7 @@ export const ALL_SHOES = [
     {
         productName: 'Colourblocked Dart IDP', 
         brandName: 'one8 x PUMA',
+        categogy: 'Sneakers',
         mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/13644184/2021/11/8/1e269947-b6b6-4fe0-9368-da9dee321a591636352735691one8xPUMAMenBlackWhiteColourblockedDartIDPSneakers2.jpg',
         price: 3699,
         smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/13644184/2021/11/8/7a1fccac-b5d9-4f14-8b27-a3914cb6042b1636352735701one8xPUMAMenBlackWhiteColourblockedDartIDPSneakers3.jpg',
@@ -75,6 +83,7 @@ export const ALL_SHOES = [
     {
         productName: 'Regular Sneakers', 
         brandName: 'ALDO',
+        categogy: 'Sneakers',
         mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/16192706/2021/11/22/9be8d0e0-a61f-4b3a-b3cb-73b8c11fc3251637560203906ALDOMenBrownColourblockedSneakers2.jpg',
         price: 1229,
         smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/16192706/2021/11/22/8938f632-aafa-486e-8a8f-23600053a37c1637560203724ALDOMenBrownColourblockedSneakers4.jpg',
@@ -84,6 +93,7 @@ export const ALL_SHOES = [
     {
         productName: 'High Top Snow Boots', 
         brandName: 'Quechua',
+        categogy: 'Boots',
         mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/13177056/2020/12/8/fe88ade0-1815-4f38-97ba-c496744586f71607419291132-Womens-Waterproof-Warm-Snow-Boots---SH500-X-WARM-LACETS---Hi-1.jpg',
         price: 3299,
         smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/13177056/2020/12/8/c9bcf29b-542c-460d-b266-4575d8ff1ec21607419291027-Womens-Waterproof-Warm-Snow-Boots---SH500-X-WARM-LACETS---Hi-3.jpg',
@@ -93,6 +103,7 @@ export const ALL_SHOES = [
     {
         productName: 'Brown Solid Formal Loafers', 
         brandName: 'Provogue',
+        categogy: 'Formal',
         mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15082082/2021/8/20/7d8c1155-96b7-4b7e-8704-9b000683e6761629460954210ProvogueMenTanBrownSolidFormalLoafers1.jpg',
         price: 1299,
         smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/15082082/2021/8/20/3145f1b8-5615-4725-8736-05e6c2ee205d1629460954760ProvogueMenTanBrownSolidFormalLoafers4.jpg',
@@ -102,6 +113,7 @@ export const ALL_SHOES = [
     {
         productName: 'Design Slip-On Sneakers', 
         brandName: 'DressBerry',
+        categogy: 'Sneakers',
         mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/13882818/2021/11/8/d468ec1e-498b-4f95-9da6-79257e87e58d1636363680451DressBerryWomenPinkWovenDesignSlip-OnSneakers1.jpg',
         price: 799,
         smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/13882818/2021/11/8/1e772842-aa0e-45eb-9799-df0def3583571636363680468DressBerryWomenPinkWovenDesignSlip-OnSneakers2.jpg',
