7f2c5c90e4cce3f68a4ad4b3939d6f4241b250d8
Syed Khutub <syedkhutubsks@gmail.com>
2021-12-02 11:16:06 +0530

    created Categories constants

---
+++ src/constants/catgeories/index.js
@@ -0,0 +1,18 @@
+export const CATEGORIES = [
+    {
+        value: 'Flip-Flop',
+        label: 'Flip-Flop'
+    },
+    {
+        value: 'Sneakers',
+        label: 'Sneakers'
+    },
+    {
+        value: 'Lace-up Shoes',
+        label: 'Lace-up Shoes',
+    },
+    {
+        value: 'Shoe Accessesories',
+        label: 'Shoe Accessesories'
+    },
+]
