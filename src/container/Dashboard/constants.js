621c63e2ba60ac02b9250d5393664820dc92a860
Syed Khutub <syedkhutubsks@gmail.com>
2021-12-02 14:24:52 +0530

    created shoes data

---
+++ src/container/Dashboard/constants.js
@@ -0,0 +1,107 @@
+export const ALL_SHOES = [
+    {
+        productName: 'Heeled Boots', 
+        brandName: 'ZAPATOZ',
+        mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/877b9154-5a71-4919-9559-30c1cc5aa8781597959110028-1.jpg',
+        price: 699,
+        smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/7f1400a4-62d4-4c9e-8795-2e3ee80523541597959110116-3.jpg',
+        smallImage2: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/5e644fcb-0a58-47ef-a5c3-aba896d691581597959110074-2.jpg'
+    },
+    {
+        productName: 'Zeta Long Distance', 
+        brandName: 'Puma',
+        mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2021/6/4/fcb19968-5d47-4879-b337-6757e4e0ee5c1622826840336-1.jpg',
+        price: 1864,
+        smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2021/6/4/1e14238f-b8e9-42df-816f-78f07f17e8341622826840344-2.jpg',
+        smallImage2: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2021/6/4/13164025-358c-4f42-b8fd-1143010a61661622826840353-3.jpg'
+    },
+    {
+        productName: 'Heeled Boots', 
+        brandName: 'ZAPATOZ',
+        mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/877b9154-5a71-4919-9559-30c1cc5aa8781597959110028-1.jpg',
+        price: 699,
+        smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/7f1400a4-62d4-4c9e-8795-2e3ee80523541597959110116-3.jpg',
+        smallImage2: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/5e644fcb-0a58-47ef-a5c3-aba896d691581597959110074-2.jpg'
+    },
+    {
+        productName: 'Heeled Boots', 
+        brandName: 'ZAPATOZ',
+        mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/877b9154-5a71-4919-9559-30c1cc5aa8781597959110028-1.jpg',
+        price: 699,
+        smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/7f1400a4-62d4-4c9e-8795-2e3ee80523541597959110116-3.jpg',
+        smallImage2: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/5e644fcb-0a58-47ef-a5c3-aba896d691581597959110074-2.jpg'
+    },
+    {
+        productName: 'Heeled Boots', 
+        brandName: 'ZAPATOZ',
+        mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/877b9154-5a71-4919-9559-30c1cc5aa8781597959110028-1.jpg',
+        price: 699,
+        smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/7f1400a4-62d4-4c9e-8795-2e3ee80523541597959110116-3.jpg',
+        smallImage2: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/5e644fcb-0a58-47ef-a5c3-aba896d691581597959110074-2.jpg'
+    },
+    {
+        productName: 'Heeled Boots', 
+        brandName: 'ZAPATOZ',
+        mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/877b9154-5a71-4919-9559-30c1cc5aa8781597959110028-1.jpg',
+        price: 699,
+        smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/7f1400a4-62d4-4c9e-8795-2e3ee80523541597959110116-3.jpg',
+        smallImage2: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/5e644fcb-0a58-47ef-a5c3-aba896d691581597959110074-2.jpg'
+    },
+    {
+        productName: 'Heeled Boots', 
+        brandName: 'ZAPATOZ',
+        mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/877b9154-5a71-4919-9559-30c1cc5aa8781597959110028-1.jpg',
+        price: 699,
+        smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/7f1400a4-62d4-4c9e-8795-2e3ee80523541597959110116-3.jpg',
+        smallImage2: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/5e644fcb-0a58-47ef-a5c3-aba896d691581597959110074-2.jpg'
+    },
+    {
+        productName: 'Heeled Boots', 
+        brandName: 'ZAPATOZ',
+        mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/877b9154-5a71-4919-9559-30c1cc5aa8781597959110028-1.jpg',
+        price: 699,
+        smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/7f1400a4-62d4-4c9e-8795-2e3ee80523541597959110116-3.jpg',
+        smallImage2: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/5e644fcb-0a58-47ef-a5c3-aba896d691581597959110074-2.jpg'
+    },
+    
+    {
+        productName: 'Heeled Boots', 
+        brandName: 'ZAPATOZ',
+        mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/877b9154-5a71-4919-9559-30c1cc5aa8781597959110028-1.jpg',
+        price: 699,
+        smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/7f1400a4-62d4-4c9e-8795-2e3ee80523541597959110116-3.jpg',
+        smallImage2: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/5e644fcb-0a58-47ef-a5c3-aba896d691581597959110074-2.jpg'
+    },
+    {
+        productName: 'Heeled Boots', 
+        brandName: 'ZAPATOZ',
+        mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/877b9154-5a71-4919-9559-30c1cc5aa8781597959110028-1.jpg',
+        price: 699,
+        smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/7f1400a4-62d4-4c9e-8795-2e3ee80523541597959110116-3.jpg',
+        smallImage2: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/5e644fcb-0a58-47ef-a5c3-aba896d691581597959110074-2.jpg'
+    },
+    {
+        productName: 'Heeled Boots', 
+        brandName: 'ZAPATOZ',
+        mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/877b9154-5a71-4919-9559-30c1cc5aa8781597959110028-1.jpg',
+        price: 699,
+        smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/7f1400a4-62d4-4c9e-8795-2e3ee80523541597959110116-3.jpg',
+        smallImage2: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/5e644fcb-0a58-47ef-a5c3-aba896d691581597959110074-2.jpg'
+    },
+    {
+        productName: 'Heeled Boots', 
+        brandName: 'ZAPATOZ',
+        mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/877b9154-5a71-4919-9559-30c1cc5aa8781597959110028-1.jpg',
+        price: 699,
+        smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/7f1400a4-62d4-4c9e-8795-2e3ee80523541597959110116-3.jpg',
+        smallImage2: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/5e644fcb-0a58-47ef-a5c3-aba896d691581597959110074-2.jpg'
+    },
+    {
+        productName: 'Heeled Boots', 
+        brandName: 'ZAPATOZ',
+        mainImage: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/877b9154-5a71-4919-9559-30c1cc5aa8781597959110028-1.jpg',
+        price: 699,
+        smallImage1: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/7f1400a4-62d4-4c9e-8795-2e3ee80523541597959110116-3.jpg',
+        smallImage2: 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/productimage/2020/8/21/5e644fcb-0a58-47ef-a5c3-aba896d691581597959110074-2.jpg'
+    },
+]
